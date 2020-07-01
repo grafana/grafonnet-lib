954e0087ec3d42b137d64cd4b2174a88d7921f6e
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2020-06-30 23:23:12 -0600

    Create cy.createDashboardFromUnitTests function
    Shared logic for creating dashboards for testing panels.

--- e2e/cypress/integration/gauge_panel_spec.js
+++ e2e/cypress/integration/gauge_panel_spec.js
@@ -1,25 +1,11 @@
-const fs = require('fs')
-
 describe('Gauge Panel', function() {
 
   let panelTitles = []
 
   before(function() {
-    cy.readFile('./tests/gauge_panel/test_compiled.json').then(function(str) {
-      let panels = []
-      for (let [i, [name, panel]] of Object.entries(Object.entries(str))) {
-        panel['id'] = parseInt(i)
-        panel['gridPos'] = {'w': 6, 'h': 4, 'x': i * 6 % 24 }
-        panelTitles.push(panel.title)
-        panels.push(panel)
-      }
-      let dashboardJSON = {
-        "uid": "gauge-panel",
-        "title": "Gauge Panel",
-        "panels": panels
-      }
-      cy.createDashboard(dashboardJSON)
-    })
+    let testDir = './tests/gauge_panel/test_compiled.json'
+    let uid = 'gauge-panel'
+    panelTitles = cy.createDashboardFromUnitTests(testDir, uid)
   })
 
   it('renders all gauge panels', function() {
--- e2e/cypress/integration/stat_panel_spec.js
+++ e2e/cypress/integration/stat_panel_spec.js
@@ -5,21 +5,9 @@ describe('Stat Panel', function() {
   let panelTitles = []
 
   before(function() {
-    cy.readFile('./tests/stat_panel/test_compiled.json').then(function(str) {
-      let panels = []
-      for (let [i, [name, panel]] of Object.entries(Object.entries(str))) {
-        panel['id'] = parseInt(i)
-        panel['gridPos'] = {'w': 6, 'h': 4, 'x': i * 6 % 24 }
-        panelTitles.push(panel.title)
-        panels.push(panel)
-      }
-      let dashboardJSON = {
-        "uid": "stat-panel",
-        "title": "Stat Panel",
-        "panels": panels
-      }
-      cy.createDashboard(dashboardJSON)
-    })
+    let testDir = './tests/stat_panel/test_compiled.json'
+    let uid = 'stat-panel'
+    panelTitles = cy.createDashboardFromUnitTests(testDir, uid)
   })
 
   it('renders all stat panels', function() {
