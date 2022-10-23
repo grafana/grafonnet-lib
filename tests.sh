8c55e08413c8551f91e4ef6d3dfcb731c359303c
Russell Howe <rhowe@siksai.co.uk>
2022-10-23 11:03:38 +0100

    Make tests.sh pass shellcheck

--- tests.sh
+++ tests.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
-jsonnet_fmt='-n 2 --max-blank-lines 2 --sort-imports --string-style s --comment-style s'
-x=0
+set -eu
 
-for i in `find . -name '*.jsonnet' -or -name '*.libsonnet'`
+jsonnet_fmt_args=(-n 2 --max-blank-lines 2 --sort-imports --string-style s --comment-style s)
+x=0
+action=${1:-test}
+
+while IFS= read -r -d '' i
 do
     t="Formatting $i..."
-    if [[ "$1" == "update" ]]; then
-        jsonnetfmt -i $jsonnet_fmt $i
+    if [[ "$action" == "update" ]]; then
+        jsonnetfmt -i "${jsonnet_fmt_args[@]}" "$i"
     fi
-    if jsonnetfmt --test $jsonnet_fmt $i;
+	 if jsonnetfmt --test "${jsonnet_fmt_args[@]}" "$i";
     then
-        echo $t OK
+        echo "$t OK"
     else
-        echo $t NOK
+        echo "$t NOK"
         x=1
     fi
-done
+done < <(find . \( -name '*.jsonnet' -or -name '*.libsonnet' \) -print0)
 
 for i in tests/*/*.jsonnet examples/*.jsonnet
 do
-    json=$(dirname $i)/$( basename $i .jsonnet )_test_output.json
-    json_e=$(dirname $i)/$( basename $i .jsonnet )_compiled.json
+    json=$(dirname "$i")/$( basename "$i" .jsonnet )_test_output.json
+    json_e=$(dirname "$i")/$( basename "$i" .jsonnet )_compiled.json
     t="Compiling $i..."
-    if jsonnet  -J . $i > $json
+    if jsonnet -J . "$i" > "$json"
     then
-        echo $t OK
+        echo "$t OK"
     else
-        echo $t NOK
+        echo "$t NOK"
         x=1
         continue
     fi
 
-    if [[ "$1" == "update" ]]; then cp $json $json_e; fi
+    if [[ "$action" == "update" ]]; then cp "$json" "$json_e"; fi
 
     t="Checking $i..."
-    if diff -urt $json $json_e
+    if diff -urt "$json" "$json_e"
     then
-        echo $t OK
+        echo "$t OK"
     else
-        echo $t NOK
+        echo "$t NOK"
         x=1
     fi
 done
