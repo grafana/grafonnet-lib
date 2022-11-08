da54d8b963be5a6f9cbecff8f9d10c4051733cc0
sam boyer <sdboyer@grafana.com>
2022-11-08 04:57:07 -0500

    poc: Add Go-based generator of JSON Schema

---
+++ grafonnext/go.mod
@@ -0,0 +1,25 @@
+module github.com/grafana/grafonnet-lib/grafonnext
+
+go 1.19
+
+require (
+	cuelang.org/go v0.4.3
+	github.com/grafana/grafana v1.9.2-0.20221107230407-93589c483967
+	github.com/grafana/thema v0.0.0-20221107225215-00ad2949c7bc
+)
+
+require (
+	github.com/cockroachdb/apd/v2 v2.0.2 // indirect
+	github.com/emicklei/proto v1.10.0 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/google/wire v0.5.0 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
+	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/protocolbuffers/txtpbfmt v0.0.0-20220428173112-74888fd59c2b // indirect
+	github.com/yalue/merged_fs v1.2.2 // indirect
+	golang.org/x/net v0.0.0-20220909164309-bea034e7d591 // indirect
+	golang.org/x/text v0.3.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ grafonnext/mkschema.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"cuelang.org/go/cue/ast"
+	"github.com/grafana/grafana/pkg/cuectx"
+	"github.com/grafana/grafana/pkg/plugins/pfs/corelist"
+	"github.com/grafana/grafana/pkg/registry/corekind"
+	"github.com/grafana/thema/encoding/jsonschema"
+)
+
+// this is a meaningless hack, we have to work out versioning
+var grafanaVersion = "latest"
+
+var rt = cuectx.GrafanaThemaRuntime()
+
+func main() {
+	// All core kinds first
+	base := filepath.Join("schemas", grafanaVersion, "core")
+	die(os.MkdirAll(base, 0777))
+
+	for _, kind := range corekind.NewBase(nil).AllStructured() {
+		f, err := jsonschema.GenerateSchema(kind.Lineage().Latest())
+		if err != nil {
+			die(fmt.Errorf("%s: %w", kind.Name(), err))
+		}
+
+		b := marshalindent(f)
+		path := filepath.Join(base, kind.MachineName()+".json")
+		die(os.WriteFile(path, b, 0644))
+	}
+
+	// Now, all composable kinds from plugins. (Note that core grafana hasn't been refactored
+	// to use 'composable kind' naming for its various methods yet)
+	for _, ptree := range corelist.New(nil) {
+		for slot, lin := range ptree.RootPlugin().SlotImplementations() {
+			f, err := jsonschema.GenerateSchema(lin.Latest())
+			if err != nil {
+				// There's a bug in generating openapi from Thema with imports are loaded.
+				// Normally an edge case in Thema, but needed in Grafana. Commenting this out
+				// until bug is fixed and just dropping errors instead
+				// die(fmt.Errorf("%s: %w", ptree.RootPlugin().Meta().Id, err))
+				continue
+			}
+
+			b := marshalindent(f)
+			basecompo := filepath.Join("schemas", grafanaVersion, "composable", strings.ToLower(slot))
+			path := filepath.Join(basecompo, ptree.RootPlugin().Meta().Id+".json")
+			die(os.MkdirAll(basecompo, 0777))
+			die(os.WriteFile(path, b, 0644))
+		}
+	}
+}
+
+func die(err error) {
+	if err != nil {
+		fmt.Fprint(os.Stderr, err, "\n")
+		os.Exit(1)
+	}
+}
+
+func marshalindent(f *ast.File) []byte {
+	b, _ := rt.Context().BuildFile(f).MarshalJSON()
+	nb := new(bytes.Buffer)
+	die(json.Indent(nb, b, "", "  "))
+	return nb.Bytes()
+}
